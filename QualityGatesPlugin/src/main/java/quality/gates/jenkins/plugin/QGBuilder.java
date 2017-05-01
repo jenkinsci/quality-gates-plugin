8653fd7d2830e125b263e633186257147d26918a
ashu16815 <ashu16815@gmail.com>
2017-05-01 23:37:18 +0530

    Build issue fixed

--- QualityGatesPlugin/src/main/java/quality/gates/jenkins/plugin/QGBuilder.java
+++ QualityGatesPlugin/src/main/java/quality/gates/jenkins/plugin/QGBuilder.java
@@ -3,6 +3,7 @@ package quality.gates.jenkins.plugin;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
+import hudson.model.Result;
 import hudson.tasks.Builder;
 import jenkins.model.GlobalConfiguration;
 import org.kohsuke.stapler.DataBoundConstructor;
@@ -47,20 +48,23 @@ public class QGBuilder extends Builder {
     }
 
     @Override
-    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+    public boolean perform(AbstractBuild build, Launcher launcher, BuildListener listener) {
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace(listener.getLogger());
         }
-		
-        boolean buildHasPassed;
         try {
             JobConfigData checkedJobConfigData = jobConfigurationService.checkProjectKeyIfVariable(jobConfigData, build, listener);
-            buildHasPassed = buildDecision.getStatus(globalConfigDataForSonarInstance, checkedJobConfigData);
+            Result buildStatus = buildDecision.getStatus(globalConfigDataForSonarInstance, checkedJobConfigData);
+            boolean buildHasPassed = buildStatus == Result.SUCCESS || buildStatus == Result.UNSTABLE;
             if("".equals(jobConfigData.getSonarInstanceName()))
                 listener.getLogger().println(JobExecutionService.DEFAULT_CONFIGURATION_WARNING);
-            listener.getLogger().println("Build-Step: Quality Gates plugin build passed: " + String.valueOf(buildHasPassed).toUpperCase());
+            listener.getLogger().println("Build-Step: Quality Gates plugin build passed: " 
+                + String.valueOf(buildHasPassed).toUpperCase());
+            if (buildStatus == Result.UNSTABLE) {
+            	build.setResult(Result.UNSTABLE);
+            }
             return buildHasPassed;
         }
         catch (QGException e){
